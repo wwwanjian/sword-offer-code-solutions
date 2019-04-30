20a2a3b8f0054b309b944b7777c31bb558adeebd
wwwanjian <wanj11th@ustbsmartcar.org>
2019-04-30 09:46:56 +0800

    update to 15

--- java/src/Problem_14.java
+++ java/src/Problem_14.java
@@ -9,11 +9,11 @@
 public class Problem_14 {
     public static void main(String args[]) {
         //获取输入
-        int n = 20;
+        int n = 50;
         int m = 4;
         //获取结果
         long start = System.nanoTime();
-        int maxMutil = getMaxMuti(n, m);
+        long maxMutil = getMaxMuti(n, m);
         long end = System.nanoTime();
         System.out.println("总共用时(ns):" + (end - start));
         //打印结果
@@ -21,19 +21,19 @@ public class Problem_14 {
     }
 
     //获取最大乘积
-    public static int getMaxMuti(int length, int pices) {
+    public static long getMaxMuti(int length, int pices) {
         //输入判断
         if (length <= 1 || pices <= 1) {
             return 0;
         }
-        int maxMuti = 0;
+        long maxMuti = 0;
         //得到乘积
         maxMuti = getMaxMuti_Solution_1(length, pices);
         return maxMuti;
     }
 
-    //动态规划解法，从上往下分解问题，从下往上求解
-    public static int getMaxMuti_Solution_1(int length, int pices) {
+    //动态规划解法，从上往下分解问题，从下往上求解 n=50时 17600ns
+    public static long getMaxMuti_Solution_1(int length, int pices) {
         //输入判断
         if (length < 2) {
             return 0;
@@ -43,15 +43,15 @@ public class Problem_14 {
             return 2;
         }
         //分解问题，把最大乘积保存在矩阵中然后取出来即可，避免重复计算
-        int[] maxMartin = new int[length + 1];
+        long[] maxMartin = new long[length + 1];
         maxMartin[0] = 0;
         maxMartin[1] = 1;
         maxMartin[2] = 2;
         maxMartin[3] = 3;
         for (int i = 4; i <= length; ++i) {
-            int max = 0;
+            long max = 0;
             for (int j = 1; j <= i / 2; ++j) {
-                int temp = maxMartin[j] * maxMartin[i - j];
+                long temp = maxMartin[j] * maxMartin[i - j];
                 if (max < temp) {
                     max = temp;
                 }
@@ -60,5 +60,28 @@ public class Problem_14 {
         }
         return maxMartin[length];
     }
-    //TODO 贪婪解法
+
+    //贪婪解法 n=50:23000ns TODO 原理不是很清楚
+    public static long getMaxMuti_Solution_2(int length, int pices) {
+        //输入判断
+        if (length < 2) {
+            return 0;
+        } else if (length == 2) {
+            return 1;
+        } else if (length == 3) {
+            return 2;
+        }
+        //尽量分成3的几段
+        long result = 1;
+        int last = 1;
+        int numOf3 = length / 3;
+        if (length % 3 == 1) {
+            --numOf3;
+            last = 4;
+        } else if (length % 3 == 2) {
+            last = 2;
+        }
+        result = (int)Math.pow(3, numOf3)*last;
+        return result;
+    }
 }
---
+++ java/src/Problem_15.java
@@ -0,0 +1,30 @@
+/**
+ * @author wwwanjian
+ * @className Problem_15
+ * @description TODO
+ * @create 2019/4/29 23:35
+ * @vsersion 1.0
+ **/
+//输出整数的二进制中1的个数
+public class Problem_15 {
+    public static void main(String args[]) {
+        //获取输入
+        int n = 50;
+        //获取结果
+        long start = System.nanoTime();
+
+        long end = System.nanoTime();
+        System.out.println("总共用时(ns):" + (end - start));
+        //打印结果
+        System.out.println(maxMutil);
+    }
+
+    //获得二进制中1的个数
+    public static int getTimesOfOne(int n){
+        //转化为二进制
+
+        //转化为字符串
+
+        //计算1的个数
+    }
+}
